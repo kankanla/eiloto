4935b4cff0480e02345e57a1cb721e9b18aa52b3
kankanla <koyi@hotmail.com>
2021-03-07 20:36:52 +0900

    start

---
+++ help.php
@@ -0,0 +1,20 @@
+<!-- 
+    mysql ダウンロード
+    mysql-8.0.23-winx64.zip
+
+    初期化
+    mysqld --initialize
+    サービス登録
+    mysqld --install MYSQL80
+    サービス起動
+    NET START MYSQL80
+
+    初期パスワード下記のファイルにある
+    C:\mysql864\data\DESKTOP30c.err
+    [Note] [MY-010454] [Server] A temporary password is generated for root@localhost: !i0i1<%P_<hu
+    パスワード変更
+    SET PASSWORD = 'toor'
+
+    MySQLの使い方
+    https://www.dbonline.jp/mysql/
+ -->
